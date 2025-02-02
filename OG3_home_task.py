81786f6f0d4c47ecd0324882822cab0bfd766530
yuproper <yuproper@gmail.com>
2025-02-02 12:42:46 +0700

    OG3 home task for vev
    Новая ветка dev

--- OG3_home_task.py
+++ OG3_home_task.py
@@ -4,3 +4,15 @@
 
     def get_name(self):
         return self._name
+		
+	def __init__(self, user_id, name):
+        super().__init__(user_id, name)
+        self._access_level = 'admin'  # Специфический уровень доступа для администраторов
+        self._users = []  # Список пользователей, управляемых администратором
+
+    def add_user(self, user):
+        if isinstance(user, User):
+            self._users.append(user)
+            print(f"Пользователь {user.get_name()} добавлен.")
+        else:
+            print("Ошибка: только объекты User могут быть добавлены.")
